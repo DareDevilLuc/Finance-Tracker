ed6b7c407f9d808fc860cb2bd122308072b125e4
DareDevilLuc <kscasirayan@up.edu.ph>
2026-06-18 15:11:49 +0800

    dialog working

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,14 @@
 import { ref, onMounted } from 'vue'
 import { supabase } from './utils/supabase'
 import Card from 'primevue/card'
-import Button from 'primevue/button';
-import DatePicker from 'primevue/datepicker';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-
+import Button from 'primevue/button'
+import DatePicker from 'primevue/datepicker'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import Dialog from 'primevue/dialog'
 
 const date = ref()
-
-
-
-
+const visible = ref(false)
 </script>
 
 <!-- TODO: each date has its own table to show expenses or allowances -->
@@ -22,37 +19,42 @@ const date = ref()
     <div class="top-section">
       <Card>
         <template #title>Current Amount (PHP)</template>
-        <template #content>
-
-        </template>
+        <template #content> </template>
       </Card>
       <Card>
         <template #title>Amount Spent (PHP)</template>
-        <template #content>
-
-        </template>
+        <template #content> </template>
       </Card>
     </div>
 
     <div class="data-section">
       <Card>
         <template #content>
-          <DatePicker v-model="date" showIcon fluid iconDisplay="input" />
+          <div class="month-button">
+            <DatePicker
+              v-model="date"
+              showIcon
+              fluid
+              iconDisplay="input"
+              dateFormat="mm/yy"
+              view="month"
+            />
+            <Button label="Add Allowance/Expense" @click="visible = true" />
+          </div>
         </template>
       </Card>
 
       <Card>
         <template #content>
-          <DataTable tableStyle="min-width: 50rem">
-          </DataTable>
-
+          <DataTable tableStyle="min-width: 50rem"> </DataTable>
         </template>
       </Card>
-
     </div>
-
-
   </div>
+
+  <Dialog v-model:visible="visible" modal header="Add Allowance/Expense">
+    Testing the bilat
+  </Dialog>
 </template>
 
 <style>
@@ -72,6 +74,12 @@ const date = ref()
 .data-section {
   display: flex;
   flex-direction: row;
+  gap: 1rem;
+}
+
+.month-button {
+  display: flex;
+  flex-direction: column;
   gap: 1rem;
 }
 </style>
