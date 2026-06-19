bc93f200d3eede0a021415f1ebf7529cca43cc3e
DareDevilLuc <kscasirayan@up.edu.ph>
2026-06-20 01:40:16 +0800

    auth a bit more progress

---
+++ src/composables/useAuth.ts
@@ -0,0 +1,6 @@
+import type { User } from '@supabase/supabase-js';
+import { ref }from 'vue'
+
+const user = ref<User | null>(null)
+
+
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MainPage from '@/components/MainPage.vue'
-import AuthPage from '@/components/AuthPage.vue'
+import MainPage from '@/pages/MainPage.vue'
+import AuthLayout from '@/layouts/AuthLayout.vue'
+import LoginPage from '@/pages/LoginPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: AuthPage,
+      component: AuthLayout,
+      children: [
+        {path: '', redirect: '/login'},
+        {path: 'login', component: LoginPage}
+      ]
     },
 
     {
